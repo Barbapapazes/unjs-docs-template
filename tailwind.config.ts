33a94712c78966493624e60e93df2fcc18d36dd1
barbapapazes <e.soubiran25@gmail.com>
2023-12-28 23:15:30 +0100

    chore: update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,27 @@
 import type { Config } from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default <Partial<Config>>{
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['DM Sans', 'DM Sans fallback', ...defaultTheme.fontFamily.sans]
-      },
       colors: {
-        green: {
-          50: '#EFFDF5',
-          100: '#D9FBE8',
-          200: '#B3F5D1',
-          300: '#75EDAE',
-          400: '#00DC82',
-          500: '#00C16A',
-          600: '#00A155',
-          700: '#007F45',
-          800: '#016538',
-          900: '#0A5331',
-          950: '#052e16'
-        }
-      }
-    }
-  }
+        yellow: {
+          50: '#FEFDF7',
+          100: '#FDFCEF',
+          200: '#FAF6D6',
+          300: '#F7F1BD',
+          400: '#F2E78C',
+          500: '#ECDC5A',
+          600: '#D4C651',
+          700: '#8E8436',
+          800: '#6A6329',
+          900: '#47421B',
+          950: '#2F2C12',
+        },
+      },
+      fontFamily: {
+        sans: ['Nunito', 'Nunito fallback', ...fontFamily.sans],
+      },
+    },
+  },
 }
